0d0349f2a5bae9ef9e4ee3625e601361b13c0ad3
Gary Fredericks <fredericksgary@gmail.com>
2015-12-02 12:05:48 -0600

    project.clj stuff

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject com.gfredericks/schema-bijections "0.1.0-SNAPSHOT"
-  :description ""
+  :description "A library for bijecting prismatic schemas."
   :url "https://github.com/gfredericks/schema-bijections"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [prismatic/schema "1.0.3"]
                  [prismatic/plumbing "0.5.2"]]
+  :deploy-repositories [["releases" :clojars]]
   :profiles {:dev {:dependencies [[camel-snake-kebab "0.3.2"]
                                   [org.clojure/test.check "0.9.0"]]}})
