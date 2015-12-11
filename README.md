47a5bd4349c8f7e7162086ae18869430ef3ba366
Gary Fredericks <fredericksgary@gmail.com>
2015-12-11 13:32:38 -0600

    Version 0.1.2

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A first draft of a library for bijecting prismatic schemas.
 ## Obtention
 
 ```
-[com.gfredericks/schema-bijections "0.1.1"]
+[com.gfredericks/schema-bijections "0.1.2"]
 ```
 
 ## Rationale
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.gfredericks/schema-bijections "0.1.2-SNAPSHOT"
+(defproject com.gfredericks/schema-bijections "0.1.2"
   :description "A library for bijecting prismatic schemas."
   :url "https://github.com/gfredericks/schema-bijections"
   :license {:name "Eclipse Public License"
